707998ec9937185c301fcfb420fccb03abc3a174
Rishab <42660337+rispop@users.noreply.github.com>
2023-08-04 09:35:24 -0500

    Removed fies from icr-files branch

--- src/models/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This module contains models that define various types. These models are for
- * internal use only and should not by used by users of the library.
- *
- * @module models
- */
-export * from './lis-alert-modifier-model';
--- src/models/lis-alert-modifier-model.ts
+++
@@ -1,4 +0,0 @@
-/**
- * UIkit alert class modifiers.
- */
-export type AlertModifierModel = 'primary' | 'success' | 'warning' | 'danger';
